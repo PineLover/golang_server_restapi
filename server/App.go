6c3627b54090c130d337d4a93b0a9b6d64f47fb9
pineLover <pineleaf1215@gmail.com>
2020-06-11 20:45:19 +0900

    second commit

---
+++ server/App.go
@@ -0,0 +1,12 @@
+package main
+
+type App struct {
+}
+
+func (a *App) Initialize(serverIp, dbName, collectionName string) {
+
+}
+
+func (app App) Run(port string) {
+
+}
---
+++ server/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type User struct {
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type fooHandler struct{}
+
+func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+	user.CreatedAt = time.Now()
+	data, _ := json.Marshal(user)
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, string(data))
+	//fmt.Fprint(w, "Hello world!")
+}
+
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello %s!!", name)
+}
+
+func main() {
+	fmt.Print("running")
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World")
+	})
+
+	http.HandleFunc("/bar", barHandler)
+
+	http.Handle("/foo", &fooHandler{})
+
+	http.ListenAndServe(":3001", nil)
+}
